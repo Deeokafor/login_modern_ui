1b470a35223764d2ffedf37584b1fd82945ea7c4
deeokafor <okafor.udahdivine@gmail.com>
2023-02-12 17:54:27 +0100

    redesigned polka dot backgrounds

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const toggle = document.querySelector('.dee-toggle');
 const toggle_child_1 = toggle.childNodes[1];
-let sign_in = document.querySelectorAll('.dee-sign-in-message');
+let sign_in_up = document.querySelectorAll('.dee-sign-in-message');
+
+let is_sign_up = () => {
+    if (toggle_child_1.textContent == 'sign in') {
+        console.log(`${toggle_child_1}`)
+    }
+}
 
 //this changes the state of the sign-in toggle and form title
 toggle.addEventListener('click', function () {
     //console.log(`${toggle.getAttribute('class')} clicked`);
     for (let key in sign_in) {
-        sign_in[key].textContent = 'hello World'
+        sign_in[key].textContent = 'sign up'
     }
     
     (function () {
