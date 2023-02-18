0b189ae55bb578ff2381f89149c8ddd963f2d485
deeokafor <okafor.udahdivine@gmail.com>
2023-02-18 06:56:47 +0100

    built in js functionality to make ui changes to the toggle and form title

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
 const toggleButton = document.querySelector('.dee-toggle');
+let submitButton = document.getElementById('submit-button');
+let toggleButtonHead = toggleButton.childNodes[1];
 let toggleButtonText = toggleButton.childNodes[3];
-let fullNameDiv = document.querySelector('#fullname-div');
 let formTitle = document.getElementById('form-title');
+let fullNameDiv = document.querySelector('#fullname-div');
+
+console.log(submitButton)
+
 
 //check text in login and form titles
 function changeToggleButtonText() {
+    //changes text to sign up
     if (toggleButtonText.textContent == 'sign in') {
         toggleButtonText.textContent = 'sign up';
-        formTitle.textContent = 'sign up';
+        formTitle.textContent = 'sign up'; 
+        removeClass(fullNameDiv, 'tw-hidden');
+        removeClass(toggleButtonHead, 'tw-order-2');
+        removeClass(toggleButton, 'tw-pr-0');
+        removeClass(toggleButton, 'tw-pl-');
+        removeClass(toggleButton, 'tw-justify-end');
         // console.log(`toggle button is ${toggleButtonText.textContent}`);
         return;
     }
 
+    //changes text to sign in
     if (toggleButtonText.textContent == 'sign up') {
         toggleButtonText.textContent = 'sign in';
         formTitle.textContent = 'sign in';
+        addClass(fullNameDiv, 'tw-hidden');
+        addClass(toggleButtonHead, 'tw-order-2');
+        addClass(toggleButton, 'tw-pr-0');
+        addClass(toggleButton, 'tw-pl-');
+        addClass(toggleButton, 'tw-justify-end');
         // console.log(`toggle button is ${toggleButtonText.textContent}`);
         return;
     }
 }
 
-//handles adding classlists
+//handles adding predefined css classes to an element's classlists
 function addClass(element, className) {
-    element.classList.add(className);
+    return element.classList.add(className);
 }
 
-//handles removing classlists
+//handles removing predefined css classes from an element's classlists
 function removeClass(element, className) {
-    element.classList.remove(className)
+    return element.classList.remove(className)
 }
 
-
-/* 
-    this changes the state of the sign-in toggle and form title 
-*/
-
+//changes the textcontent of the toggle button and form title
 toggleButton.addEventListener('click', function() {
     changeToggleButtonText();
     console.log(`toggle button clicked`);
+});
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
 });
 
